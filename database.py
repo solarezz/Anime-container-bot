1d376d3ffeca146e2185c4a05d6c21b3980e0d32
solarezz <122013283+solarezz@users.noreply.github.com>
2024-07-29 15:33:39 +0500

    main

--- database.py
+++ database.py
@@ -1,4 +1,13 @@
 import sqlite3
 
-connection = sqlite3.connect('C:\Python\', check_same_thread=False)
+connection = sqlite3.connect('C:\Python\Anime-container-bot\database.db', check_same_thread=False)
 cursor = connection.cursor()
+
+def table_input(user_id: int, username: str):
+    cursor.execute('INSERT INTO users (user_id, username) VALUES (?, ?)', (user_id, username))
+    connection.commit()
+
+def info(user_id: int):
+    cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
+    result = cursor.fetchone()
+    return result
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+import database as db
 
 bot = telebot.TeleBot("6588713567:AAGoPx0v6rCbndSkgGN6rhCwapOTFdpks2c")
 
@@ -11,6 +12,7 @@ def start_message(message):
     bot.reply_to(message, f"""\
     Привет, {message.from_user.first_name}. Я бот - Аниме Контейнер. Можешь мне отправить список аниме, которых ты уже просмотрел(а) и я добавлю их в твою базу данных.\n\
 В этой базе данных будет отображаться: количество просмотренных аниме и средний рейтинг каждого аниме.""", reply_markup=markup)
+    db.table_input(message.chat.id, message.from_user.username)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
@@ -19,8 +21,8 @@ def callback_handler(call):
         bot.edit_message_text(chat_id=call.message.chat.id,
                               message_id=call.message.message_id,
                               text=f"""
-{call.message.from_user.first_name}, Ваш профиль:
-ID:
-Количество просмотренных аниме: """)
+@{db.info(call.message.chat.id)[2]}, Ваш профиль:
+ID: {db.info(call.message.chat.id)[0]}
+Количество просмотренных аниме: {db.info(call.message.chat.id)[3]}""")
 
 bot.infinity_polling()
